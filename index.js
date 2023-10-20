cf3dcfda6d3b77495681f401371692bd333d5430
d1-spec <martinlazarte962@gmail.com>
2023-10-20 16:04:05 -0300

    arranged and organized code

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const getCompChoice = function () {
-
-    const opciones = ["Rock", "Paper", "Scissors" ];
-
-    const opAleatoria = Math.floor(Math.random() * 3);
-    return opciones[opAleatoria];
-}
- 
-
-function game (computerSelection, playerSelection) {
-  computerSelection = getCompChoice().toLowerCase();
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == computerSelection) {
-    console.log("Tie");
-    return "Tie";
-  } else if (
-    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Paper" && computerSelection == "Rock") ||
-    (playerSelection == "scissors" && computerSelection == "Paper")
-  ) {
-    console.log("You win!");
-              return "You win!";
-              } else {
-                console.log("You lose.");
-                return "You lose";
-              }
-}
-
-const player = "Paper";
-
-game(getCompChoice, player)
-=======
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
@@ -91,4 +58,3 @@ scissors.addEventListener('click', function () {
     const computerSelection = getCompChoice();
     game(player, computerSelection);
 })
->>>>>>> rps
